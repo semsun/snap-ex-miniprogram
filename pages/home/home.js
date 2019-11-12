6e9bc79674aadfa8a83fa157eec9c8cf0aecd835
SunYongQian <semsun@warickmedical.com>
2019-11-12 14:18:15 +0800

    Add Bottom Navgation bar

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,14 +5,14 @@ Page({
    * Page initial data
    */
   data: {
-
+    
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
